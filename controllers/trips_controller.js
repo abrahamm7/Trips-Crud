87d967cf90b82606af0f784c34f4b6410fe76ed5
Abraham <abrahammorillo777@gmail.com>
2024-03-11 00:07:07 -0400

    fix some issues

--- controllers/trips_controller.js
+++ controllers/trips_controller.js
@@ -1,11 +1,13 @@
 const express = require("express");
+require("dotenv").config();
 const sql = require("mssql/msnodesqlv8");
+const trip_model = require("../models/trip_model.js");
 const app = express();
 
 app.listen(3000, () => console.log("Listening on port 3000"));
 
 app.get("/", async (req, res) => {
-  res.send("Trips controller");
+  res.send(`Database name is ${process.env.DB_NAME}`);
 });
 
 app.get("/trips", async (req, res) => {
@@ -25,5 +27,16 @@ app.get("/trips", async (req, res) => {
       .then((result) => {
         res.send(result.recordset);
       });
+  });
+});
+
+app.post("/addTrip", (req, res) => {
+  var connection = new sql.ConnectionPool({
+    database: "TripsDB",
+    server: "LAPTOP-3Q354RTQ\\SQLEXPRESS",
+    driver: "msnodesqlv8",
+    options: {
+      trustedConnection: true,
+    },
   });
 });
