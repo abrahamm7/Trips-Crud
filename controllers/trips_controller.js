401fbdec48afcda2ac11e7b0237127b2b242c9fb
Abraham <abrahammorillo777@gmail.com>
2024-03-11 16:22:41 -0400

    add put controller

--- controllers/trips_controller.js
+++ controllers/trips_controller.js
@@ -2,6 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const sql = require("mssql/msnodesqlv8");
 const app = express();
+const connection = new sql.ConnectionPool({
+  database: "TripsDB",
+  server: "LAPTOP-3Q354RTQ\\SQLEXPRESS",
+  driver: "msnodesqlv8",
+  options: {
+    trustedConnection: true,
+  },
+});
 
 app.use(express.json());
 
@@ -13,16 +21,7 @@ app.get("/", async (req, res) => {
   res.send(`Database name is ${process.env.DB_NAME}`);
 });
 
-app.get("/trips", async (req, res) => {
-  // config for your database
-  var connection = new sql.ConnectionPool({
-    database: "TripsDB",
-    server: "LAPTOP-3Q354RTQ\\SQLEXPRESS",
-    driver: "msnodesqlv8",
-    options: {
-      trustedConnection: true,
-    },
-  });
+app.get("/gettrips", async (req, res) => {
   connection.connect().then(() => {
     connection
       .request()
@@ -34,19 +33,20 @@ app.get("/trips", async (req, res) => {
 });
 
 app.post("/addTrip", async (req, res) => {
-  var connection = new sql.ConnectionPool({
-    database: "TripsDB",
-    server: "LAPTOP-3Q354RTQ\\SQLEXPRESS",
-    driver: "msnodesqlv8",
-    options: {
-      trustedConnection: true,
-    },
-  });
-
   connection.connect().then(async () => {
     const { Destination, StartDate, EndDate, Description } = req.body;
     const result = await connection.request()
       .query`INSERT INTO Trips (Destination, StartDate, EndDate, Description) VALUES (${Destination}, ${StartDate}, ${EndDate}, ${Description})`;
+    res.send(`New trip added ${result}`);
+  });
+});
+
+app.put("/updateTrip", async (req, res) => {
+  connection.connect().then(async () => {
+    const { TripID, Destination, StartDate, EndDate, Description } = req.body;
+    const result = await connection.request()
+      .query`UPDATE Trips SET Destination = ${Destination}, StartDate = ${StartDate}, EndDate = ${EndDate}, Description = ${Description}
+      WHERE TripId = ${TripID}`;
     res.send(`New trip added ${result}`);
   });
 });
